82d8d9fa2276b93547e1e60ae87b395a70bdb7ab
marianoirato <marianoirato@hotmail.com>
2024-05-13 21:06:53 +0000

    Camaras funcionando en RGB y conectadas a ROS usando el paquete catkin

---
+++ camaras_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/tesis/camaras_ws/src
+# Build directory: /home/pi/tesis/camaras_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("camaras")
