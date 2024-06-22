09cc25cb753b006fa291edf3363d7728670c32a7
UrielCarte <urielcartechini@gmail.com>
2024-06-22 21:09:04 +0000

    Mejora la lecutra del codigo

--- rmo_ws/src/camaras/src/main.cpp
+++ rmo_ws/src/camaras/src/main.cpp
@@ -18,8 +18,10 @@
 #define WIDTH  640
 #define HEIGHT 480
 #define FPS 30
+
 using namespace gsttcam;
 using namespace std;
+
 typedef struct
 {
    bool ready_to_process_frame;
@@ -60,10 +62,6 @@ GstFlowReturn new_frame_cb(GstAppSink *appsink, gpointer data)
             pCustomData->ready_to_show_frame = true;
             pCustomData->ready_to_process_frame = false;
         }
-        else
-        {
-            cout<< "asd"<<endl;
-        }
     }
 
     gst_buffer_unmap(buffer, &info);
@@ -72,13 +70,23 @@ GstFlowReturn new_frame_cb(GstAppSink *appsink, gpointer data)
     return GST_FLOW_OK;
 }
 
+void process_frame(CUSTOMDATA &customData, image_transport::Publisher &image_pub)
+{
+    if (customData.ready_to_show_frame)
+    {
+        sensor_msgs::ImagePtr image = cv_bridge::CvImage(std_msgs::Header(), "bgr8", customData.frame).toImageMsg();
+        image_pub.publish(image);
+        customData.ready_to_show_frame = false;
+    }
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "camaras_node");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
-    image_transport::Publisher image_pub1 = it.advertise("/camera1", 1);
-    image_transport::Publisher image_pub2 = it.advertise("/camera2", 1);
+    image_transport::Publisher image_pub1 = it.advertise("/camera1", 10);
+    image_transport::Publisher image_pub2 = it.advertise("/camera2", 10);
 
     gst_init(&argc, &argv);
 
@@ -90,14 +98,11 @@ int main(int argc, char **argv)
     CustomData2.ready_to_process_frame = true;
     CustomData2.ready_to_show_frame = false;
 
-    ros::Rate loop_rate(10);
-
-// { //INICIALIZACION DE LAS CAMARAS
+    //INICIALIZACION DE LAS CAMARAS
     std::vector<CameraInfo> camera_list = get_device_list();
     if (camera_list.size() < 2)
     {
-        printf("At least two cameras are required.\n");
-        //return 1;
+        printf("Conectar las dos camaras.\n");
     }
 
     std::string serialnumber1 = camera_list[0].serial;
@@ -116,7 +121,8 @@ int main(int argc, char **argv)
     cam2.set_capture_format("BGRx", FrameSize{WIDTH, HEIGHT}, FrameRate{FPS, 1});
     cam2.set_new_frame_callback(new_frame_cb, &CustomData2);
     cam2.start();
-// }
+
+    ros::Rate loop_rate(FPS * 2);
 
     while (ros::ok())
     {
@@ -125,38 +131,17 @@ int main(int argc, char **argv)
         ros::spinOnce();
         loop_rate.sleep();
 
-        if (CustomData1.ready_to_show_frame)
-        {
-            cv::Mat frame_copy = CustomData1.frame.clone();
-
-            std::vector<uchar> buf;
-            cv::imencode(".jpg", frame_copy, buf);  // Compress the image
-            cv::Mat compressed_frame = cv::imdecode(buf, cv::IMREAD_COLOR);
-
-            sensor_msgs::ImagePtr image1 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", compressed_frame).toImageMsg();
-            image_pub1.publish(image1);
-            CustomData1.ready_to_show_frame = false;
-        }
-        
-        if (CustomData2.ready_to_show_frame)
-        {
-            cv::Mat frame_copy = CustomData2.frame.clone();
-
-            std::vector<uchar> buf;
-            cv::imencode(".jpg", frame_copy, buf);  // Compress the image
-            cv::Mat compressed_frame = cv::imdecode(buf, cv::IMREAD_COLOR);
-
-            sensor_msgs::ImagePtr image2 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", compressed_frame).toImageMsg();
-            image_pub2.publish(image2);
-            CustomData2.ready_to_show_frame = false;
-        }
+        process_frame(CustomData1, image_pub1);
+        process_frame(CustomData2, image_pub2);
     }
 
     printf("Press Enter to end the program");
     getchar();
 
     cam1.stop();
-    //cam2.stop();
+    cam2.stop();
 
     return 0;
 }
+
+
